2cdb38710daca25c36604711330fb3732ec02f4a
ish <ismael.veyna@gmail.com>
2021-11-12 14:25:31 -0500

    api calls are working through AllData, need them to spit out the content to innerHTML

--- src/pages/AllData.js
+++ src/pages/AllData.js
@@ -1,6 +1,15 @@
 import { useContext } from 'react';
+import { isCompositeComponentWithType } from 'react-dom/cjs/react-dom-test-utils.production.min';
 
 import UserContext from '../UserContext.js';
+
+async function callAPI(){
+  
+  let response = await fetch("http://localhost:5000/account/all")
+  let data = await response.json();
+  console.log(JSON.stringify(data.body))
+  return data.body
+}
 
 function AllData() {
 
@@ -10,7 +19,7 @@ function AllData() {
     <div className='content'>
       <h2>All Data</h2>
       <div className='json'>
-        {JSON.stringify(context, null, 2)}
+        {JSON.stringify(callAPI())}
       </div>
     </div>
   );
