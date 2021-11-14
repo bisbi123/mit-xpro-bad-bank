a32222656d97b7b5b28fe9bfa7b19e0b822076a1
ish <ismael.veyna@gmail.com>
2021-11-14 14:11:03 -0500

    removed the data abstraction layer from AllData and put it back in dal.ks

--- src/pages/AllData.js
+++ src/pages/AllData.js
@@ -1,27 +1,23 @@
-import { useContext } from 'react';
-import { isCompositeComponentWithType } from 'react-dom/cjs/react-dom-test-utils.production.min';
 
-import UserContext from '../UserContext.js';
+import {getAllUsersAsArray} from '../dal'
 
 function callAPI(){
-  
-  fetch("http://localhost:5000/account/all")
-    .then(response => response.json())
-      .then(data =>{
-        // console.log(JSON.stringify(data));
+
+  getAllUsersAsArray()
+    .then(arrOut =>{
+      arrOut.forEach(data => {
         let el = document.getElementById('allData');
-        data.forEach(element => {
-          // console.log(JSON.stringify(element))
-          el.innerHTML += JSON.stringify(element);
-        });;
-      }
-    );
+        el.innerHTML += JSON.stringify(data);
+        
+      });
+    })
+  
+
 }
 
 function AllData() {
 
-  const context = useContext(UserContext);
-  // callAPI();
+  
   return (
     <div className='content'>
       <h2>All Data</h2>
