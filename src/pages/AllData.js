97502a4819b2c46fe67d1e98c0c5cf1674ae95af
ish <ismael.veyna@gmail.com>
2021-11-14 13:25:48 -0500

    finally getting that data!

--- src/pages/AllData.js
+++ src/pages/AllData.js
@@ -3,24 +3,33 @@ import { isCompositeComponentWithType } from 'react-dom/cjs/react-dom-test-utils
 
 import UserContext from '../UserContext.js';
 
-async function callAPI(){
+function callAPI(){
   
-  let response = await fetch("http://localhost:5000/account/all")
-  let data = await response.json();
-  console.log(JSON.stringify(data.body))
-  return data.body
+  fetch("http://localhost:5000/account/all")
+    .then(response => response.json())
+      .then(data =>{
+        // console.log(JSON.stringify(data));
+        let el = document.getElementById('allData');
+        data.forEach(element => {
+          // console.log(JSON.stringify(element))
+          el.innerHTML += JSON.stringify(element);
+        });;
+      }
+    );
 }
 
 function AllData() {
 
   const context = useContext(UserContext);
-
+  // callAPI();
   return (
     <div className='content'>
       <h2>All Data</h2>
-      <div className='json'>
-        {JSON.stringify(callAPI())}
+      <button onClick={callAPI}>Get Data</button>
+      <div className="container">
+        <div id='allData' className="content"/>
       </div>
+      
     </div>
   );
 }
