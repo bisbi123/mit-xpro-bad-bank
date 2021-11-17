d7cecf977987bdfb3bd2332456b772ef5d0faf64
ish <ismael.veyna@gmail.com>
2021-11-16 19:28:11 -0500

    not sure how this DAL got borked up

--- src/dal.js
+++ src/dal.js
@@ -1,88 +1,88 @@
-// const MongoClient = require('mongodb').MongoClient;
-// const url         = 'mongodb://localhost:27017';
-// let db            = null;
+const MongoClient = require('mongodb').MongoClient;
+const url         = 'mongodb://localhost:27017';
+let db            = null;
  
-// // connect to mongo
-// MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
-//     console.log("Connected successfully to db server");
+// connect to mongo
+MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
+    console.log("Connected successfully to db server");
 
-//     // connect to local database
-//     db = client.db('local');
-// });
+    // connect to myproject database
+    db = client.db('local');
+});
 
-// // create user account
-// function createAccount(name, email, password){
-//     return new Promise((resolve, reject) => {    
-//         const collection = db.collection('users');
-//         const doc = {name, email, password, balance: 0};
-//         collection.insertOne(doc, {w:1}, function(err, result) {
-//             err ? reject(err) : resolve(doc);
-//         });    
-//     })
-// }
-
-// // find user account
-// function find(email){
-//     return new Promise((resolve, reject) => {    
-//         const customers = db
-//             .collection('users')
-//             .find({email: email})
-//             .toArray(function(err, docs) {
-//                 err ? reject(err) : resolve(docs);
-//         });    
-//     })
-// }
-
-// // find user account
-// function findAccount(email){
-//     return new Promise((resolve, reject) => {    
-//         const customers = db
-//             .collection('users')
-//             .findOne({email: email})
-//             .then((doc) => resolve(doc))
-//             .catch((err) => reject(err));    
-//     })
-// }
-
-// // update - deposit/withdraw amount
-// function addFunds(email, amount){
-//     return new Promise((resolve, reject) => {    
-//         const customers = db
-//             .collection('users')            
-//             .findOneAndUpdate(
-//                 {email: email},
-//                 { $inc: { balance: amount}},
-//                 { returnOriginal: false },
-//                 function (err, documents) {
-//                     err ? reject(err) : resolve(documents);
-//                 }
-//             );            
-
-
-//     });    
-// }
-
-// // all users
-// function findAllUsers(){
-//     return new Promise((resolve, reject) => {    
-//         const customers = db
-//             .collection('users')
-//             .find({})
-//             .toArray(function(err, docs) {
-//                 err ? reject(err) : resolve(docs);
-//         });    
-//     })
-// }
-
-
-// module.exports = {createAccount, findAccount, find, addFunds, findAllUsers};
-
-/**
- * Return an array of users in the database
- * @returns {Promise} An array wrapped in an promise
- */
-export function getAllUsersAsArray(){
-    return fetch("http://localhost:5000/account/all")
-      .then(response => response.json())
+// create user account
+function create(name, email, password){
+    return new Promise((resolve, reject) => {    
+        const collection = db.collection('users');
+        const doc = {name, email, password, balance: 0};
+        collection.insertOne(doc, {w:1}, function(err, result) {
+            err ? reject(err) : resolve(doc);
+        });    
+    })
 }
+
+// find user account
+function find(email){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('users')
+            .find({email: email})
+            .toArray(function(err, docs) {
+                err ? reject(err) : resolve(docs);
+        });    
+    })
+}
+
+// find user account
+function findOne(email){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('users')
+            .findOne({email: email})
+            .then((doc) => resolve(doc))
+            .catch((err) => reject(err));    
+    })
+}
+
+// update - deposit/withdraw amount
+function update(email, amount){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('users')            
+            .findOneAndUpdate(
+                {email: email},
+                { $inc: { balance: amount}},
+                { returnOriginal: false },
+                function (err, documents) {
+                    err ? reject(err) : resolve(documents);
+                }
+            );            
+
+
+    });    
+}
+
+// all users
+function all(){
+    return new Promise((resolve, reject) => {    
+        const customers = db
+            .collection('users')
+            .find({})
+            .toArray(function(err, docs) {
+                err ? reject(err) : resolve(docs);
+        });    
+    })
+}
+
+
+module.exports = {create, findOne, find, update, all};
+
+// /**
+//  * Return an array of users in the database
+//  * @returns {Promise} An array wrapped in an promise
+//  */
+// export function getAllUsersAsArray(){
+//     return fetch("http://localhost:5000/account/all")
+//       .then(response => response.json())
+// }
 
