b590477e06d74a9a4a3a47eed85a812fd44838a8
ish <ismael.veyna@gmail.com>
2021-11-27 15:21:55 -0500

    create account logic added

--- src/pages/CreateAccount.js
+++ src/pages/CreateAccount.js
@@ -1,62 +1,111 @@
-import { useContext } from 'react';
+import React from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 
-import UserContext from '../UserContext.js';
+var clicked = false;
 
-function CreateAccount() {
+/**
+ * Ensure that we don't deposit pre-maturely
+ */
+function onCreate(){
+  clicked = true;
+}
 
-  const context = useContext(UserContext);
-
-  const formikProps = {
-    initialValues: {
-      username: '',
-      password: ''
-    },
-    validate: values => {
-      const errors = {};
-      if (!values.username) {
-        errors.username = 'Required';
+async function createNewAccount(name, email, password) {
+  if (!clicked){
+    return [false, null];
+  }
+  else {
+    clicked = false;
+    let resp = await fetch(`http://localhost:5000/account/create/${name}/${email}/${password}`);
+    if (resp.status === 200){
+      let data = await resp.json();
+      if (data !== null){
+        if (data.email === email & data.password === password){
+          return [true, data];
+        }
+        else {return [false, null];}
       }
-      if (!values.password) {
-        errors.password = 'Required';
-      }
-      return errors;
-    },
-    onSubmit: (values, { resetForm }) => {
-      context.users.push({
-        username: values.username,
-        password: values.password,
-        balance: 0
-      });
-      resetForm();
-      alert(`Account created with username: ${values.username}`);
     }
-  };
+  }
+  
 
-  return (
-    <div className='content'>
-      <h2>Create Account</h2>
-      
-      <Formik {...formikProps}>
-        <Form>
-          <div className='form-group'>
-            <label htmlFor='username'>Username</label>
-            <Field className='form-control' id='username' name='username' placeholder='acrist' />
-            <ErrorMessage className='error' name='username' component='div' />
-          </div>
+}
 
-          <div className='form-group'>
-            <label htmlFor='password'>Password</label>
-            <Field className='form-control' id='password' name='password' placeholder='****' type='password' />
-            <ErrorMessage className='error' name='password' component='div' />
-          </div>
-          
-          <br/>
-          <button type='submit' className='btn btn-primary'>Create</button>
-        </Form>
-      </Formik>
-    </div>
-  );
+class CreateAccount extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.formikProps = {
+      initialValues: {
+        name: '',
+        email: '',
+        password: ''
+      },
+      validate: async values => {
+        const errors = {};
+        if (!values.name) {
+          errors.name = 'Required';
+        }
+        else if (!values.email) {
+          errors.email = 'Required';
+        }
+        else if (!values.password) {
+          errors.password = 'Required';
+        }
+        else {
+          // let us attempt to create this sucker
+          const accountCreated = await createNewAccount(
+            values.name,
+            values.email,
+            values.password
+          )
+        }
+        
+
+        
+
+
+        return errors;
+
+      },
+      onSubmit: (values, { resetForm }) => {
+        resetForm();
+        alert(`Welcome ${values.name}\nYour email account is: ${values.email}`);
+      }
+    };
+  }
+  render() {
+    return (
+      <div className='content'>
+        <h2>Create Account</h2>
+        
+        <Formik {...this.formikProps}>
+          <Form>
+            <div className='form-group'>
+              <label htmlFor='name'>Name</label>
+              <Field className='form-control' id='name' name='name' placeholder='John Doe' />
+              <ErrorMessage className='error' name='name' component='div' />
+            </div>
+
+            <div className='form-group'>
+              <label htmlFor='email'>Email</label>
+              <Field className='form-control' id='email' name='email' placeholder='john.doe@email.com' />
+              <ErrorMessage className='error' name='email' component='div' />
+            </div>
+  
+            <div className='form-group'>
+              <label htmlFor='password'>Password</label>
+              <Field className='form-control' id='password' name='password' placeholder='****' type='password' />
+              <ErrorMessage className='error' name='password' component='div' />
+            </div>
+            
+            <br/>
+            <button type='submit' className='btn btn-primary' onClick={onCreate}>Create</button>
+          </Form>
+        </Formik>
+      </div>
+    );
+  }
 }
 
 export default CreateAccount;
