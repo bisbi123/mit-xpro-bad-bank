a8cc14cc57fc42272553b28780d328a85a637f65
ish <ismael.veyna@gmail.com>
2021-11-30 20:35:39 -0500

    re-enable api and mongo db and update DAL to point to mongodb cluster

--- backend/dal.js
+++ backend/dal.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb://mongo:27017/';
+const url         = 'mongodb+srv://one-week-user-starts-2021-11-30:njJcxLRn1xzudixp@bbdb-cluster.yvexm.mongodb.net';
 let db            = null;
  
 // connect to mongo
@@ -7,13 +7,13 @@ MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
     console.log("Connected successfully to db server");
 
     // connect to myproject database
-    db = client.db('local');
+    db = client.db('bbdb-cluster');
 });
 
 // create user account
 function createAccount(name, email, password){
     return new Promise((resolve, reject) => {    
-        const collection = db.collection('users');
+        const collection = db.collection('customers');
         const doc = {name, email, password, balance: 0};
         collection.insertOne(doc, {w:1}, function(err, result) {
             err ? reject(err) : resolve(doc);
@@ -25,7 +25,7 @@ function createAccount(name, email, password){
 function findAccount(email){
     return new Promise((resolve, reject) => {    
         const customers = db
-            .collection('users')
+            .collection('customers')
             .find({email: email})
             .toArray(function(err, docs) {
                 err ? reject(err) : resolve(docs);
@@ -37,7 +37,7 @@ function findAccount(email){
 function findOne(email){
     return new Promise((resolve, reject) => {    
         const customers = db
-            .collection('users')
+            .collection('customers')
             .findOne({email: email})
             .then((doc) => resolve(doc))
             .catch((err) => reject(err));    
@@ -48,7 +48,7 @@ function findOne(email){
 function updateBalance(email, amount){
     return new Promise((resolve, reject) => {    
         const customers = db
-            .collection('users')            
+            .collection('customers')            
             .findOneAndUpdate(
                 {email: email},
                 { $inc: { balance: amount}},
@@ -66,7 +66,7 @@ function updateBalance(email, amount){
 function findAllAccounts(){
     return new Promise((resolve, reject) => {    
         const customers = db
-            .collection('users')
+            .collection('customers')
             .find({})
             .toArray(function(err, docs) {
                 err ? reject(err) : resolve(docs);
